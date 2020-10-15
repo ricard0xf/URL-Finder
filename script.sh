588c2e6f6cf3547b6881b3cfbf7640030d3e3d76
José Ricardo <67844149+ricardobap15@users.noreply.github.com>
2020-10-14 23:14:36 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,28 +4,28 @@ if [ "$1" == "" ]
 then
 	echo "Script by R1C4RD0"
 	echo "$0 [DOMÍNIO]"
-	echo "Ex: $0 google.com"
+	echo "Ex: $0 exemplo.com"
 else
 	echo "========================================================================"
-	echo "		             [+] URLs em: $1"
+	echo "		          [+] URLs em: $1"
 	echo "========================================================================"
 
-wget $1 2>/dev/null  
+wget $1 2>/dev/null
 
-cat index.html | grep "href=\"http" | cut -d "/" -f 3 | cut -d "\"" -f 1 | cut -d "?" -f 1 | grep -v "<" | sort -u  > urls
+cat index.html | grep "href=\"http" | cut -d "/" -f 3 | cut -d "\"" -f 1 | cut -d "?" -f 1 | grep -v "<" | sort -u  > /tmp/urls
 
-cat urls
+cat /tmp/urls
 echo "========================================================================="
 
-for url in $(cat urls); do  host $url; done | grep "has address" | sort -u  > ips
+for url in $(cat /tmp/urls); do  host $url; done | grep "has address" | sort -u  > /tmp/ips
 
-echo "		             [+] IPs em: $1"
+echo "		          [+] IPs em: $1"
 echo "========================================================================="
-cat ips
+cat /tmp/ips
 echo "========================================================================="
 
-rm index.html* 
-# remove o "html" que foi baixado, cuidado se você tiver outro arquivo com o mesmo nome!
+rm index.html
+# Remove o download do HTML da página para evitar acumulações. Certifique-se de sempre ver se não tem outro arquivo importante com o mesmo nome!
 
 fi
  
